f909f1a82266f9bb3e2584d4b6aaf9fb3351463a
unknown <sanahanji>
2018-09-19 23:30:29 +0530

    added the intent feature

--- app/src/main/java/com/example/abhi/quiz/First_Ques.java
+++ app/src/main/java/com/example/abhi/quiz/First_Ques.java
@@ -9,9 +9,20 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 public class First_Ques extends MainActivity {
+    private Button b;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first);
+        b=(Button)findViewById(R.id.button2);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(First_Ques.this,Second_Ques.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
